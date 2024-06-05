b6fd543d0a3ff05a2512466012f11af88a3cacbf
Viktor Rengel <viktorengel@hotmail.com>
2024-06-04 23:50:51 -0500

    Imagenes y Css estáticos

--- resources/views/_components/card.blade.php
+++ resources/views/_components/card.blade.php
@@ -1,4 +1,5 @@
 <div style="border: 1px solid black; border-radius: 5px; padding: 10px; margin: 5px;">
     <h3>{{ $title }}</h3>
+    <img src="{{ asset('assets/img/w3.png') }}" alt="" width="250px">
     <p>{{ $content }}</p>
 </div>
